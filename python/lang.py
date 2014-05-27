b914220ba86a60cf7a147035c94db05885253785
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2014-05-27 10:34:24 +0200

    added language detection examples

---
+++ python/lang.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+Detect the language of a text
+
+API-Key and text are submitted in JSON body of request
+
+"""
+
+import sys
+import json
+
+try:
+    import requests
+except:
+    print "We require the excellent requests library! Try: pip install requests"
+
+
+if len(sys.argv)<2:
+    print "Usage: iptc.py <api-key>"
+    sys.exit(1)
+
+api_key = sys.argv[1]
+
+no_text = """Sør-Afrika: Takket være et mål i siste liten av Sami Khedira, tok
+Tyskland bronsen under Fotball-VM 2010 i Sør-Afrika."""
+
+
+
+response = requests.post('http://api.orbitapi.com/lang',
+                         data=json.dumps(dict(api_key=api_key, text=no_text)),
+                         headers = {'content-type': 'application/json'})
+
+if response.status_code == requests.codes.ok:
+    print json.dumps(response.json(), sort_keys=True, indent=4, separators=(',', ': '))
+else:
+    print "Error", response.status_code, response.text
